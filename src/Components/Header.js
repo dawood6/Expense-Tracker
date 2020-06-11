22d6fc78994041abea56f2e226cae1021c49e9df
dawood6 <dawoodsadiq171@gmail.com>
2020-06-11 17:53:22 +0500

    feat: expense racker by constext api nad react hooks

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h2>
+            Expense Tracker By Dawood Sadiq
+        </h2>
+    )
+}
+
+export default Header
---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './Components/Header';
+import Balance from './Components/Balance';
+import IncomeExpense from './Components/IncomeExpense';
+import TransactionList from './Components/TransactionList';
+import AddTransaction from './Components/AddTransaction';
+
+import { GlobalProvider } from './Components/context/GlobalState';
+
+import './app.css';
+
+function App() {
+    return (
+        <GlobalProvider>
+            <Header />
+            <div className="container">
+                <Balance />
+                <IncomeExpense />
+                <TransactionList />
+                <AddTransaction />
+            </div>
+        </GlobalProvider>
+    );
+}
+
+export default App;
